57faa761681fa036c9d92d9ff02cab611c8559ce
Master <blinkerion@gmail.com>
2022-09-16 01:06:44 -0500

    final update

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ for (let i = 0; i < buttonArray.length; i++) {
 
         makeSound(buttonInnerHtml)
 
+        buttonAnimation(buttonInnerHtml)
+
         });
 }
 
@@ -18,6 +20,8 @@ document.addEventListener("keydown" , function(event) {
 
     makeSound(event.key)
     
+    buttonAnimation(event.key)
+
 })
 
 function makeSound(key) {
@@ -65,6 +69,17 @@ function makeSound(key) {
 }
 
 
+function buttonAnimation (currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey)
+
+    activeButton.classList.add("pressed")
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100)
+
+}
 
 
 
